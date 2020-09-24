8d7a1e4ee6f03b10d5eda7fc6b3a0ed226b3359e
Knyazev <knyazev741@icloud.com>
2020-09-24 22:02:42 +0300

    added 6 exercise

---
+++ 6.py
@@ -0,0 +1,13 @@
+line = input("Введите слово с маленькой буквы: ")
+
+def int_func(word):
+    first = []
+    for i in range(len(word)):
+        if i == 0:
+            first.append(word[0].upper())
+        else:
+            first.append(word[i])
+    return ''.join(first)
+
+
+print(int_func(line))
